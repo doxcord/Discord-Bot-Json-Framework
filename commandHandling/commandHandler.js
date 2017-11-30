45d157e9641c78b72637563bef2844031fe1f18a
doxcord <34086989+doxcord@users.noreply.github.com>
2017-11-30 04:13:33 -0500

    Update commandHandler.js

--- commandHandling/commandHandler.js
+++ commandHandling/commandHandler.js
@@ -1,5 +1,16 @@
 const cmds = require('../commands')
 
 new Handler ({
-
+  for(ping in cmds) {
+    cmds = ['Framework']
+    if(Framework == true) {
+      cmds.push()
+    }
 });
+ 
+/**
+_______________NOTE______________________
+               ^^^^
+/* This is a unfished command handler */
+  
+/**
